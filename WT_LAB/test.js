c2f9c3fbeffeed1db73d89fc004d9fe33e67dff0
KrishnaJi-Thegreat <guptakrishna07349@gmail.com>
2026-01-27 12:12:28 +0530

    new file of js

---
+++ WT_LAB/test.js
@@ -0,0 +1,19 @@
+console.log("hello world")
+
+const v = 20.23456
+console.log(typeof v)
+
+const v2 = "Kanha"
+console.log(typeof v2)
+
+const s1 = Symbol("symbol")
+console.log(typeof s1)
+
+const b3 = true
+console.log(typeof b3)
+
+const n4 = null
+console.log(typeof n4)
+
+let v6
+console.log(typeof v6)
